0f8da1a3c2bec0230ec7b0a18fa7320cfacb2fcc
It'smenaatt <nadiantshva31@gmail.com>
2025-03-25 16:14:02 +0700

    Update and rename m4.2.py to To do list.py

--- Minggu 4/To do list.py
+++ Minggu 4/To do list.py
@@ -1,22 +1,3 @@
-import math
-
-def hitung_akar_kuadrat():
-    while True:
-        try:
-            angka = input("Masukkan angka: ")
-            angka = float(angka)
-            
-            if angka < 0:
-                print("Input tidak valid. Harap masukkan angka positif.")
-            elif angka == 0:
-                print("Error: Akar kuadrat dari nol tidak diperbolehkan.")
-            else:
-                hasil = math.sqrt(angka)
-                print(f"Akar kuadrat dari {angka} adalah {hasil}")
-                break
-        except ValueError:
-            print("Input tidak valid. Harap masukkan angka yang valid.")
-
 def todo_list():
     tugas = []
     while True:
